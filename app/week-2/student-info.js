6a9d3fb3f89d346a25cdc8bfc94d069ef30c6e1f
Daisy-Di-2024 <daisy050718@gmail.com>
2025-05-13 10:41:29 -0600

    I completed assignment 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function StudentInfo(){
+    return(
+        <main>
+            <p>My name: Daisy Di</p>
+            <Link href="https://github.com/Daisy-Di-2024?tab=repositories">https://github.com/Daisy-Di-2024?tab=repositories</Link>
+        </main>
+    )
+}
